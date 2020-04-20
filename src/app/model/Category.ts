85871c5594c9375abc1656c92ba1f89d3e729542
bravemaster619 <bravemaster619@hotmail.com>
2020-04-20 14:47:21 +0800

    feat(model): add category interface

--- src/app/model/Category.ts
+++ src/app/model/Category.ts
@@ -3,3 +3,12 @@ export interface MenuCategory {
   name: string;
   nameEN: string;
 }
+
+export interface CategoryInterface {
+  _id: string;
+  name: string;
+  nameEN?: string;
+  description?: string;
+  descriptionEN?: string;
+  parentId?: string|null;
+}
