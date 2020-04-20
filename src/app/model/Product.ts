fc7345acd619031e01888896392ed7ad20b13d09
bravemaster619 <bravemaster619@hotmail.com>
2020-04-20 14:46:24 +0800

    feat(model): add attribute interface to product model

--- src/app/model/Product.ts
+++ src/app/model/Product.ts
@@ -10,16 +10,36 @@ export interface FeaturedProductInterface {
   }>,
   order: number
 }
-
+export interface ProductAttributeInterface {
+  name: string,
+  nameEN?: string,
+  values: Array<{
+    name: string,
+    nameEN?: string,
+    price: number,
+    quantity: number
+  }>
+}
 export interface ProductInterface {
   _id: string,
   name: string,
+  nameEN?: string,
   description: string,
+  descriptionEN?: string,
   price: number,
   featured: number,
+  dow? : Array<any>,
   pictures: Array<{
     name: string,
     url: string
   }>,
-  order: number
+  order: number,
+  status?: boolean,
+  stock?: {
+    enabled: boolean,
+    quantity?: number,
+    outofstockMessage?: string,
+    outofstockMessageEN?: string
+  },
+  attributes: Array<any>
 }
