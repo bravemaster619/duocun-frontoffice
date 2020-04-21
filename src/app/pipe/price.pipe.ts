02d32fe8a8c12087d6f653408eee786d78c72962
bravemaster619 <bravemaster619@hotmail.com>
2020-04-21 14:07:18 +0800

    fix(pipe): change pure to false

--- src/app/pipe/price.pipe.ts
+++ src/app/pipe/price.pipe.ts
@@ -1,7 +1,8 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'price'
+  name: 'price',
+  pure: false
 })
 export class PricePipe implements PipeTransform {
   transform(value: any, currency: string = '$'): string {
