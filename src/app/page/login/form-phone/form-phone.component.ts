46486a39149c023974e5bf40d68707ddbd3344a0
bravemaster619 <bravemaster619@hotmail.com>
2020-04-23 20:37:08 +0800

    feat(auth): implement signup flow

--- src/app/page/login/form-phone/form-phone.component.ts
+++ src/app/page/login/form-phone/form-phone.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, ActivatedRoute } from '@angular/router';
+import { ApiService } from 'src/app/service/api/api.service';
+import { AlertController } from '@ionic/angular';
+import { TranslateService } from '@ngx-translate/core';
+import { AuthenticationService } from 'src/app/service/authentication/authentication.service';
 @Component({
   selector: 'app-form-phone',
   templateUrl: './form-phone.component.html',
@@ -8,13 +12,71 @@ import { Component, OnInit } from '@angular/core';
 export class FormPhoneComponent implements OnInit {
 
   isOtpSent: boolean
-
-  constructor() { 
-    this.isOtpSent = false;
+  failedCount: number;
+  processing: boolean;
+  phone: string;
+  otp: string;
+  returnUrl: string;
+  constructor(
+    private api: ApiService,
+    private alert: AlertController,
+    private translator: TranslateService,
+    private auth: AuthenticationService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) { 
+    
   }
 
   ngOnInit() {
-    
+    this.isOtpSent = false;
+    this.failedCount = 0;
+    this.processing = false;
+    this.returnUrl = this.activatedRoute.snapshot.queryParams['returnUrl'] || '/';
+  }
+
+  handleLogin() {
+    this.api.v1().post("Accounts/loginByPhone", {phone: this.phone, verificationCode: this.otp}).then(observable => {
+      observable.subscribe((token: string) => {
+        if (token) {
+          this.auth.login(token).then(() => {
+            this.router.navigate([this.returnUrl]);
+          });
+        } else {
+          this.showAlert("Notice", "Login failed", "OK");
+          this.failedCount++;
+        }
+      })
+    }).catch(e => {
+      this.showAlert("Notice", "Login failed", "OK");
+      this.failedCount++;
+    })
+  }
+
+  handleSendOTP() {
+    this.api.v2().post("users/send-otp", { phone: this.phone }).then(observable => {
+      observable.subscribe((data: any) => {
+        if (data.success) {
+          this.showAlert("Notice", "Verification code sent", "OK");
+        } else {
+          this.showAlert("Notice", "Verification code not sent", "OK");
+        }
+      })
+    });
+  }
+
+  showAlert(header, message, button) {
+    this.translator.get([header, message, button]).subscribe(dict => {
+      this.alert.create({
+        header: dict[header],
+        message: dict[message],
+        buttons: [dict[button]]
+      }).then(alert => alert.present());
+    });
+  }
+
+  isValid(phone:string):boolean {
+    return phone && phone.length > 7 && phone.length < 15
   }
 
 }
--- src/app/page/register/register.page.ts
+++ src/app/page/register/register.page.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { TranslateService } from '@ngx-translate/core';
-
+import { Router, ActivatedRoute } from '@angular/router';
+import { ApiService } from 'src/app/service/api/api.service';
+import { AlertController } from '@ionic/angular';
+import { AuthenticationService } from 'src/app/service/authentication/authentication.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
@@ -11,17 +14,118 @@ export class RegisterPage implements OnInit {
 
   isPhoneVerified: boolean
 
+  phone: string;
+  verificationCode: string;
+  username: string;
+  password: string;
+  passwordConfirmation: string;
+
   constructor(
     private title: Title,
-    private translator: TranslateService
+    private translator: TranslateService,
+    private api: ApiService,
+    private alert: AlertController,
+    private auth: AuthenticationService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
   ) {
     this.isPhoneVerified = false;
+    this.phone = "";
+    this.verificationCode = "";
+    this.username = "";
+    this.password = "";
+    this.passwordConfirmation = "";
   }
 
   ngOnInit() {
     this.translator.get("title.signup").subscribe((title: string) => {
       this.title.setTitle(title);
-    })
+    });
   }
+
+  isInputValid(): boolean {
+    if (!this.phone || this.phone.length < 7 || this.phone.length > 15) {
+      return false;
+    }
+    if (!this.verificationCode) {
+      return false;
+    }
+    if (this.username) {
+      if (this.username.length < 5 || this.username.length > 255) {
+        return false;
+      }
+    }
+    if (this.password) {
+      if (this.password.length < 6 || this.password.length > 255) {
+        return false;
+      }
+      if (this.password !== this.passwordConfirmation) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  handleSendOTP() {
+    this.api.v2().post("users/send-otp", { phone: this.phone }).then(observable => {
+      observable.subscribe((data: any) => {
+        if (data.success) {
+          this.showAlert("Notice", "Verification code sent", "OK");
+        } else {
+          this.showAlert("Notice", "Verification code not sent", "OK");
+        }
+      })
+    });
+  }
+
+  
+  handleSignUp() {
+    this.api.v2().post("users/signup", {
+      phone: this.phone,
+      verificationCode: this.verificationCode,
+      username: this.username,
+      password: this.password
+    }).then(observable => {
+      observable.subscribe((res:any) => {
+        console.log(res);
+        if (res.success) {
+          const header = "Notice";
+          const message = "Registered successfully";
+          const button = "OK";
+          this.translator.get([header, message, button]).subscribe(dict => {
+            this.alert.create({
+              header: dict[header],
+              message: dict[message],
+              buttons: [{
+                text: dict[button],
+                handler: () => {
+                  this.router.navigate(["/login"]);
+                }
+              }]
+            }).then(alert => alert.present())
+          })
+        } else {
+          if (res.message && res.message === "verification_failed") {
+            this.showAlert("Notice", "Verification failed", "OK");
+          } else if (res.message && res.message === "username_duplicated") {
+            this.showAlert("Notice", "Username is already in use", "OK");
+          } else {
+            this.showAlert("Notice", "Registration failed", "OK");
+          }
+        }
+      });
+    }) 
+  }
+
+  showAlert(header, message, button) {
+    this.translator.get([header, message, button]).subscribe(dict => {
+      this.alert.create({
+        header: dict[header],
+        message: dict[message],
+        buttons: [dict[button]]
+      }).then(alert => alert.present());
+    });
+  }
+
 
 }
