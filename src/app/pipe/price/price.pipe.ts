e7b0670f33d99c9366ca9962c2a7d38e4b62630b
bravemaster619 <bravemaster619@hotmail.com>
2020-04-23 16:10:25 +0800

    fix(pipe): change intl option for usd currency

--- src/app/pipe/price/price.pipe.ts
+++ src/app/pipe/price/price.pipe.ts
@@ -9,17 +9,17 @@ export class PricePipe implements PipeTransform {
   formatter: any
 
   constructor() {
-    this.formatter = new Intl.NumberFormat('en-US');
+    this.formatter = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' });
   }
 
-  transform(value: any, currency: string = '$'): string {
+  transform(value: any): string {
     if (typeof value === "object") {
-      return this.transform(value.price, currency);
+      return this.transform(value.price);
     }
     value = parseFloat(<string> value);
     if (value < 0 || isNaN(value)) {
       value = 0;
     }
-    return this.formatter.format(value) + ` ${currency}`;
+    return this.formatter.format(value);
   }
 }
