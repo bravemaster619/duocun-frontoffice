1aa2b850cdb7fdebfaba4771807cc1e510180566
bravemaster619 <bravemaster619@hotmail.com>
2020-04-23 17:43:20 +0800

    feat(pipe): add moment pipe

---
+++ src/app/pipe/moment/moment.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import moment from 'moment';
+
+@Pipe({
+  name: 'moment',
+  pure: false
+})
+export class MomentPipe implements PipeTransform {
+  transform(value:any, ...args: Array<any>): any {
+    let [format] = args;
+    return moment(value).format(format);
+  }
+}
